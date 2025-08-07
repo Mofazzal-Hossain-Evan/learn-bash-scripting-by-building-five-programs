5e638163f11dda9a4e5bce82b15fe727db7a00ca
Evan <md.mofazzalhossain18@gmail.com>
2025-08-07 03:54:34 +0000

    Save progress: 822.1

--- countdown.sh
+++ countdown.sh
@@ -18,11 +18,10 @@ then
 
   while [[ $I -ge 0 ]]
   do
-  echo $I
-  (( I-- ))
-  sleep 1
+    echo $I
+    (( I-- ))
+    sleep 1
   done
-
 else
   echo Include a positive integer as the first argument.
 fi
